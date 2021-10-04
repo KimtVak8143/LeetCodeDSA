60fc848dc88c94bf88f70d4426246ce0d4c735a2
MOHIT KUMAR KUSHWAHA <mk8143@srmist.edu.in>
2021-10-04 14:19:33 +0530

    addition

--- Solutions_3/Q63.py
+++ Solutions_3/Q63.py
@@ -13,6 +13,33 @@ class Solution:
                     return new[:x]
         return new
 
+# sample 16 ms submission
+
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#
+#         # initialize the longest prefix
+#         prefix = strs[0]
+#
+#         # loop through remaining string
+#         for string in strs[1:]:
+#
+#             # if the current string is shorter than the prefix, swap the current prefix with the string
+#             if len(string) < len(prefix):
+#                 temp = string
+#                 string = prefix
+#                 prefix = temp
+#
+#             # compare the prefix with the string until an unequality and then cut the prefix and break out
+#             for i in range(len(prefix)):
+#                 if prefix[i] != string[i]:
+#                     if i > 0:
+#                         prefix = prefix[0:i]  # cut prefix
+#                         break
+#                     else:
+#                         return ""  # if the very first character does not match, then there is no common prefix
+#         return prefix
+
 
 # sample 12 ms submission
 #
