70434435a699230aa5f3297126d183d8a5371d0b
KimtVak8143 <mk8143@srmist.edu.in>
2021-11-27 19:38:28 +0530

    day1

--- Solutions_3/Q69.py
+++ Solutions_3/Q69.py
@@ -1,6 +1,26 @@
+# 326. Power of Three
+
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        for i in range(len(nums)):
-            res ^=nums[i]
-        return res
+    def isPowerOfThree(self, n: int) -> bool:
+        if n<=0:
+            return False
+        d = int(log2(n)/log2(3))
+        return pow(3, d) == n
+
+# # sample 48 ms submission
+# class Solution:
+#     def isPowerOfThree(self, n: int) -> bool:
+#         if n <= 0:
+#             return False
+#         while n > 1:
+#             if n % 3 == 0:
+#                 n = n/3
+#             else:
+#                 return False
+#         return True
+#
+# # sample 40 ms submission
+#
+# class Solution:
+#     def isPowerOfThree(self, n: int) -> bool:
+#         return n > 0 and 3**20 % n == 0
